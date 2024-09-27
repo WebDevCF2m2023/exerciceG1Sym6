9629ac3ea3063fc178e8fa21eaec57f25dbcef50
leerlandais <leerlandais@gmail.com>
2024-09-27 15:30:44 +0100

    layout improvements for public side

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main', methods: ['GET'])]
+    public function index(TagRepository $tagRepository): Response
     {
-        return $this->render('template.new.html.twig', [
-            'controller_name' => 'MainController',
+        return $this->render('main/index.html.twig', [
+            'tags' => $tagRepository->findAll(),
         ]);
     }
 }
