4fd6c85f13cb1395f73607b89d4f5bb9f5ad4d0d
Anthoonyyy <Anthony212007@hotmail.com>
2024-09-30 14:04:01 +0200

    ajout des sections dans le menu et formulaire login

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,15 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(SectionRepository $sections): Response
     {
         return $this->render('main/index.html.twig', [
             'title' => 'Accueil',
             'homepage_text' =>  "Nous sommes le ".date('d/m/Y \à H:i'),
-
+            'sections' => $sections->findAll()
         ]);
     }
     #[Route('/about', name: 'about_me')]
@@ -25,6 +26,28 @@ class MainController extends AbstractController
             'homepage_text'=> "Et je parle encore de moi !",
             # on met dans une variable pour twig toutes les sections récupérées
             'sections' => $sections->findAll()
+        ]);
+    }
+    // création de l'url pour le détail d'une section
+    #[Route(
+        # chemin vers la section avec son id
+        path: '/section/{id}',
+        # nom du chemin
+        name: 'section',
+        # accepte l'id au format int positif uniquement
+        requirements: ['id' => '\d+'],
+        # si absent, donne 1 comme valeur par défaut
+        defaults: ['id'=>1])]
+
+    public function section(SectionRepository $sections, int $id): Response
+    {
+        // récupération de la section
+        $section = $sections->find($id);
+        return $this->render('main/section.html.twig', [
+            'title' => 'Section '.$section->getSectionTitle(),
+            'homepage_text'=> $section->getSectionDescription(),
+            'section' => $section,
+            'sections' => $sections->findAll(),
         ]);
     }
 }
