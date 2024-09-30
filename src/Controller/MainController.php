c5e744acafcf406c0ae8a3d070bf545b8ba83266
Anthoonyyy <Anthony212007@hotmail.com>
2024-09-30 13:02:24 +0200

    update

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +12,19 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
-            'title' => 'Homepage',
-            'homepage_text' =>  "Nous sommes le ".date('d/m/Y \à H:i')
+            'title' => 'Accueil',
+            'homepage_text' =>  "Nous sommes le ".date('d/m/Y \à H:i'),
+
+        ]);
+    }
+    #[Route('/about', name: 'about_me')]
+    public function aboutMe(SectionRepository $sections): Response
+    {
+        return $this->render('main/about.html.twig', [
+            'title' => 'About me',
+            'homepage_text'=> "Et je parle encore de moi !",
+            # on met dans une variable pour twig toutes les sections récupérées
+            'sections' => $sections->findAll()
         ]);
     }
 }
